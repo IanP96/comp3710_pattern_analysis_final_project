a7ba91913bf600f18c3e63d93ba5e3c889f335fe
Ian <spinto774@yahoo.com>
2025-10-27 14:31:55 +1000

    add run script

---
+++ recognition/lob_timegan_Ian_Pinto_48006581/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# script to run training on UQ Rangpur
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition=a100
+#SBATCH --job-name=final_proj
+
+conda activate comp3710
+python train.py --env rangpur > output/stdout.txt 2> output/stderr.txt
