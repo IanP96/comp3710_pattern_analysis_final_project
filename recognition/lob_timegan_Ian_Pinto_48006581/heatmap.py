b15685c56173950f2210bd49c6196e700bfb09c7
Ian <spinto774@yahoo.com>
2025-10-30 16:21:01 +1000

    add ssim calculation for images

--- recognition/lob_timegan_Ian_Pinto_48006581/heatmap.py
+++ recognition/lob_timegan_Ian_Pinto_48006581/heatmap.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy.typing import NDArray
+from skimage.metrics import structural_similarity as ssim
+from skimage import img_as_float
 
 from dataset import load_data
 from constants import NUM_LEVELS
@@ -17,13 +19,34 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-def plot_heatmap(data: NDArray, plot_title: str | None = None, plot_save_path: Path | str | None = None) -> None:
+def get_ssim(path_img1: Path | str, path_img2: Path | str) -> float:
+    """Calculate SSIM between two images.
+
+    Args:
+      - path_img1: path to first image
+      - path_img2: path to second image
+
+    Returns:
+      - ssim_value: calculated SSIM value
+    """
+    img1 = img_as_float(plt.imread(path_img1))
+    img2 = img_as_float(plt.imread(path_img2))
+    print(img1.shape)
+    ssim_value = ssim(img1, img2, multichannel=True, channel_axis=2, data_range=1)
+    return ssim_value
+
+
+def plot_heatmap(
+    data: NDArray,
+    plot_title: str | None = None,
+    plot_save_path: Path | str | None = None,
+) -> None:
 
     volume_data = data[:, 1:40:2]
     max_volume = np.max(volume_data)
     # min_volume = np.min(volume_data)
     logger.debug("max_volume=%s", max_volume)
-    
+
     price_data = data[:, 0:40:2]
     max_price = np.max(price_data)
     min_price = np.min(price_data)
@@ -45,16 +68,18 @@ def plot_heatmap(data: NDArray, plot_title: str | None = None, plot_save_path: P
                 volume = row[feature_num + 1]
                 x[i] = time_point
                 y[i] = price
-                colour[i] = np.array([
-                    0.99 if order_type == ASK else 0.01,
-                    0.01,
-                    0.99 if order_type == BID else 0.01,
-                    volume / max_volume
-                ])
+                colour[i] = np.array(
+                    [
+                        0.99 if order_type == ASK else 0.01,
+                        0.01,
+                        0.99 if order_type == BID else 0.01,
+                        volume / max_volume,
+                    ]
+                )
                 if not 0 <= volume / max_volume <= 1:
                     raise ValueError(str(volume / max_volume))
                 i += 1
-    
+
     assert i == len(x) == len(y) == len(colour)
 
     plt.ylim(min_price, max_price)
@@ -69,7 +94,18 @@ def plot_heatmap(data: NDArray, plot_title: str | None = None, plot_save_path: P
 
 
 if __name__ == "__main__":
-    # heatmap on test data
+
+    # heatmap and SSIM on test data
     command_line_options = Options().parse()
     _, _, test_data = load_data(command_line_options)
-    plot_heatmap(test_data[:int(len(test_data) / 10)], "Heatmap of test LOBSTER data", "test_heatmap.png")
+    cutoff = int(len(test_data) / 100)
+    plot_heatmap(
+        test_data[:cutoff], "Heatmap of test LOBSTER data", "test_heatmap.png"
+    )
+    plot_heatmap(
+        test_data[:cutoff] + np.random.rand(cutoff, NUM_LEVELS * 4) * 100,
+        "Heatmap of test LOBSTER data plus noise",
+        "test_heatmap_noise.png",
+    )
+    test_data_ssim = get_ssim(Path("test_heatmap.png"), Path("test_heatmap_noise.png"))
+    print(f"{test_data_ssim = }")
