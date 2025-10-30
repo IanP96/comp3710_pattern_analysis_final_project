74b0c79a68cf093bd97b2a9696ac0795a8a72ba8
Ian <spinto774@yahoo.com>
2025-10-30 10:50:57 +1000

    fix length of inference generated data

--- recognition/lob_timegan_Ian_Pinto_48006581/modules.py
+++ recognition/lob_timegan_Ian_Pinto_48006581/modules.py
@@ -757,7 +757,7 @@ class TimeGAN:
 
     def run_inference(self):
         self.generated_data = self.generation(
-            self.opt.batch_size, self.test_max_val, self.test_min_val
+            len(self.test_data), self.test_max_val, self.test_min_val
         )
         GENERATED_DATA_PATH = Path(OUTPUT_DIR, "generated_data.npy")
         np.save(GENERATED_DATA_PATH, self.generated_data)
--- recognition/lob_timegan_Ian_Pinto_48006581/utils.py
+++ recognition/lob_timegan_Ian_Pinto_48006581/utils.py
@@ -31,7 +31,7 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 
 # Device configuration
