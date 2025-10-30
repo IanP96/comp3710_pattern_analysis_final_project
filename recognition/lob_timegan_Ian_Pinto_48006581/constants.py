ccce48a5c888fd1a41138a7f73e11033d09e18ce
Ian <spinto774@yahoo.com>
2025-10-30 10:27:15 +1000

    fix kl metric calculation

--- recognition/lob_timegan_Ian_Pinto_48006581/constants.py
+++ recognition/lob_timegan_Ian_Pinto_48006581/constants.py
@@ -12,7 +12,7 @@ RANGPUR_NUM_TRAINING_ITERATIONS = 30_000
 RANGPUR_VALIDATE_INTERVAL = 300
 
 # How many training iterations to use (max) running locally on laptop
-LOCAL_NUM_TRAINING_ITERATIONS = 4
+LOCAL_NUM_TRAINING_ITERATIONS = 3
 LOCAL_VALIDATE_INTERVAL = 1
 
 TRAIN_TEST_VALIDATE = (0.6, 0.2, 0.2)
@@ -22,9 +22,3 @@ assert (
 
 DATA_DIR = "data"
 ORDERBOOK_DATA_FILENAME = "AMZN_2012-06-21_34200000_57600000_orderbook_10.csv"
-
-# By inspection of the source data, midprice return is always within this range
-MPR_RANGE = (-2e-4, 2e-4)
-
-# By inspection of the source data, spread is always within this range
-SPREAD_RANGE = (0, 6000)
--- recognition/lob_timegan_Ian_Pinto_48006581/modules.py
+++ recognition/lob_timegan_Ian_Pinto_48006581/modules.py
@@ -71,7 +71,6 @@ from constants import (
 
 logging.basicConfig(
     format="%(asctime)s %(levelname)-8s %(message)s",
-    level=logging.INFO,
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 logger = logging.getLogger(__name__)
